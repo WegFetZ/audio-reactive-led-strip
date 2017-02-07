17419729e407e12eea91b87c7e7894d2288c3fc7
WegFetZ <mweber92@web.de>
2017-02-07 15:41:30 +0100

    handle audio input-stream exception

--- python/microphone.py
+++ python/microphone.py
@@ -5,25 +5,30 @@ import config
 
 
 def start_stream(callback):
-    p = pyaudio.PyAudio()
-    frames_per_buffer = int(config.MIC_RATE / config.FPS)
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=config.MIC_RATE,
-                    input=True,
-                    frames_per_buffer=frames_per_buffer)
-    overflows = 0
-    prev_ovf_time = time.time()
-    while True:
-        try:
-            y = np.fromstring(stream.read(frames_per_buffer), dtype=np.int16)
-            y = y.astype(np.float32)
-            callback(y)
-        except IOError:
-            overflows += 1
-            if time.time() > prev_ovf_time + 1:
-                prev_ovf_time = time.time()
-                print('Audio buffer has overflowed {} times'.format(overflows))
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    try:
+        p = pyaudio.PyAudio()
+        frames_per_buffer = int(config.MIC_RATE / config.FPS)
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=config.MIC_RATE,
+                        input=True,
+                        frames_per_buffer=frames_per_buffer)
+        overflows = 0
+        prev_ovf_time = time.time()
+        while True:
+            try:
+                y = np.fromstring(stream.read(frames_per_buffer), dtype=np.int16)
+                y = y.astype(np.float32)
+                callback(y)
+            except IOError:
+                overflows += 1
+                if time.time() > prev_ovf_time + 1:
+                    prev_ovf_time = time.time()
+                    print('Audio buffer has overflowed {} times'.format(overflows))
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+    except Exception as e:
+        print('Failed to start audio input-stream. Will try again in 5s: {}'.format(e.message))
+        time.sleep(5)
+        start_stream(callback)
